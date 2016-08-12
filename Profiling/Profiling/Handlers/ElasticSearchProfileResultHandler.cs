1168f8b49af6b897d45d7332bae7c73ff90bf67c
Justin Nelson <justin.john.nelson@gmail.com>
2016-08-12 09:22:36 -0500

    Implement before and after methods for elastic search

--- Profiling/Profiling/Handlers/ElasticSearchProfileResultHandler.cs
+++ Profiling/Profiling/Handlers/ElasticSearchProfileResultHandler.cs
@@ -7,7 +7,7 @@ using Nest;
 
 namespace Profiling.Handlers
 {
-    public class ElasticSearchProfileResultHandler : BaseProfileResultHandler
+    public class ElasticSearchProfileResultHandler : IProfileResultHandler
     {
         private readonly ElasticClient _elasticClient;
         private readonly string _indexName;
@@ -24,24 +24,61 @@ namespace Profiling.Handlers
             }
         }
 
-        public override void LogProfileResults(Guid session, string label, TimeSpan elapsed, string[] tags)
+        public void LogProfileResults(Guid session, string label, TimeSpan elapsed, string[] tags)
         {
             _elasticClient.Index(new ElasticProfilingResult
             {
                 Label = label,
                 MilisElapsed = elapsed.TotalMilliseconds,
                 Tags = tags,
+                SessionId = session,
+            }, ix => ix.Index(_indexName));
+        }
+
+        public void LogProfileStart(Guid session, string label, DateTime start, string[] tags)
+        {
+            LogProfileStartEnd(session, label, start, tags, "start");
+        }
+
+        public void LogProfileEnd(Guid session, string label, DateTime end, string[] tags)
+        {
+            LogProfileStartEnd(session, label, end, tags, "end");
+        }
+
+        private void LogProfileStartEnd(Guid session, string label, DateTime time, string[] tags, string milestoneString)
+        {
+            _elasticClient.Index(new ElasticProfilingStartEnd
+            {
+                Label = label,
+                Milestone = milestoneString,
+                Tags = tags,
+                Timestamp = time,
+                SessionId = session,
             }, ix => ix.Index(_indexName));
         }
     }
 
     [ElasticsearchType]
-    public class ElasticProfilingResult
+    public abstract class BaseElasticProfilingObj
     {
         [String(Index = FieldIndexOption.NotAnalyzed)]
         public string[] Tags { get; set; }
         public string Label { get; set; }
+        [String(Index = FieldIndexOption.NotAnalyzed)]
+        public Guid SessionId { get; set; }
+    }
 
+    [ElasticsearchType]
+    public class ElasticProfilingResult : BaseElasticProfilingObj
+    {
         public double MilisElapsed { get; set; }
+    }
+
+    [ElasticsearchType]
+    public class ElasticProfilingStartEnd : BaseElasticProfilingObj
+    {
+        public DateTime Timestamp { get; set; }
+        [String(Index = FieldIndexOption.NotAnalyzed)]
+        public string Milestone { get; set; }
     }
 }
